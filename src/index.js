b2df09c249d42f0780fdd4917c6f24fdbe2eec38
Maxence Machu <maxence@akarah.com>
2022-04-05 09:47:13 +0200

    refactor(jest): clean project

--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
 import './style/main.scss';
-import {callAPI, getWeatherStringFromCode, getWeatherTextFromString, renderHTML, renderOnline} from './functions'
+import {myFirstFunction} from './functions'
 
 document.addEventListener('DOMContentLoaded', async function(event) {
-    
-    renderOnline(navigator.onLine);
-
-    if(navigator.onLine){
-        let temps = await callAPI();
-    
-        let code_temps = temps.forecast.weather
-        console.info('Le code temps renvoyé par l\'API est : ', code_temps);
-    
-        let weather_string = getWeatherStringFromCode(code_temps);
-        let weather_text = getWeatherTextFromString(weather_string);
-    
-        renderHTML(weather_string, weather_text);
-    } else {
-        // Chargement des données en localStorage
-    }
-
+    let functionCall = myFirstFunction();
 })
-
-window.addEventListener('offline', renderOnline(false));
-window.addEventListener('online',renderOnline(true));
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,67 +1,30 @@
-import {callAPI, getWeatherStringFromCode, renderOnline} from '../src/functions';
+import {myFirstFunction} from '../src/functions';
 
-describe('Weather string', () => {
-    it('should return a string', () => {
-        expect(typeof getWeatherStringFromCode(0)).toBe('string')
-    })
-    it('should return a sunny', () => {
-        expect(getWeatherStringFromCode(0)).toBe('SUNNY')
-    })
-})
+describe('My first tests', () => {
+    it('should return hello world', () => {
+        let _call = myFirstFunction();
+        expect(_call).toBe('Hello World');
+    });
+}) 
 
 /*
 **Simulation du DOM**
 Jest est livré avec jsdom qui simule un environnement DOM comme si vous étiez dans le navigateur. 
 Cela signifie que chaque API du DOM que nous appelons peut être observée de la même manière qu'elle le serait dans un navigateur
 */
+
 /** @jest-environment jsdom */
 describe('DOM', () => {
 
     document.body.innerHTML =
-    '<div>' +
-    '  <span id="js-online" />'+
+    '<div id="app">' +
     '</div>';
 
-    it('should show offline', () => {  
-        renderOnline(false);
+    it('should render Hello World', () => {
 
-        let DOMInnerText = document.getElementById('js-online').innerText;
-
-        expect(DOMInnerText).toEqual('Your are offline')
-    })
-})
-
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () => Promise.resolve(),
-  })
-);
-
-describe('API', () => {
-    it('should return current weather', async () => {
-
-        fetch.mockImplementationOnce(() => Promise.resolve({
-            json: () => Promise.resolve({
-                forecast : {
-                    weather : 0
-                }
-            })
-        }));
-
-        let weather = await callAPI();
-        expect(fetch).toHaveBeenCalledTimes(1);
-        expect(typeof weather).toBe('object')
-    })
-
-    it('should return an error', async () => {
-            
-            fetch.mockImplementationOnce(() => Promise.resolve({
-                json: () => Promise.reject({
-                    message : 'error'
-                })
-            }));
-    
-            let weather = await callAPI();
-            expect(weather.message).toBe('error')
+        let _call = myFirstFunction();
+        document.getElementById('app').innerHTML = _call;
+        
+        expect(document.getElementById('app').innerHTML).toBe('Hello World');
     })
 })
